ffc67cca1f145846409b98ee52118ef42bbafa03
Kamil_Niemiec <kamil_niemiec@o2.pl>
2023-03-11 15:55:10 +0100

    message add to index.tsx file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,10 @@ import Section from "@/components/Section";
 
 import { sectionData } from "@/data/sectionData";
 
+//READ THIS: If there is any problem with size of elements please change a window of broweser to
+// 1366x768 px, it is the resolution of device I was working with but I hope the app is rendering
+//correctly on higher resolutions, I used % and vh, vw units.
+
 export default function Home() {
   return (
     <div className={styles.wrapper}>
